3c8dc7701b8ea3496650a43755a835509207368d
rubensmk <rubenskishimoto@gmail.com>
2022-08-21 19:15:05 -0300

    changes

--- pages/api/generate-og.js
+++ pages/api/generate-og.js
@@ -9,8 +9,7 @@ export default async function opengraph(req, res) {
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: { width: 1200, height: 630 },
-    // executablePath: await chromium.executablePath,
-    executablePath: 'http://localhost:3000/',
+    executablePath: await chromium.executablePath,
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
   })
