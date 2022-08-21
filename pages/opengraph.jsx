de595e5f808c803cfa14aa1a848bf3bedf642b35
rubensmk <rubenskishimoto@gmail.com>
2022-08-21 19:37:00 -0300

    changes

--- pages/opengraph.jsx
+++ pages/opengraph.jsx
@@ -6,9 +6,8 @@ export default function Opengraph() {
 
   return (
     <div className='w-[1200px] h-[630px]'>
-      <div className='flex flex-col h-full items-center justify-between px-12 py-24'>
+      <div className='flex flex-col h-2 items-center justify-center px-12 py-24'>
         <h1 className='text-7xl grow font-semibold'>{title}</h1>
-        {/* <img className='w-24 h-24 rounded-full mb-4' src={img} alt={title} /> */}
         <span className='text-3xl'>by blockchainscope.io</span>
       </div>
     </div>
